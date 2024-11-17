4067ff2859f063d0cb54bdb3a70599fa9f7db686
Admin32R2 <ralphtajantajan4@gmail.com>
2024-11-17 20:33:11 +0800

    Update index_front_page.js

--- javascript/index_front_page.js
+++ javascript/index_front_page.js
@@ -8,7 +8,7 @@
     // Check if the input matches the word 'boot' (case-insensitive)
     if (userInput === "Boot") {
         // Redirect to the new page
-        window.location.href = "../text_blank_canvas.html"; // Replace with your desired page URL
+        window.location.href = "text_blank_canvas.html"; // Replace with your desired page URL
     }
 });
 
